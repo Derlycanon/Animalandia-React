71633a0a1501b6466c9cf03935cec1e34709a5ba
derlycanon <derlycaon@gmail.com>
2022-06-08 16:17:51 -0500

    Add changes cards

---
+++ src/components/perros/perros.js
@@ -0,0 +1,70 @@
+let perros = [
+
+{
+    "id": 1,
+    "raza": "Basenji",
+    "thumbnail": "https://cdn.royalcanin-weshare-online.io/tfo_HWsBIYfdNSoC7xo7/v1/bd28bl-hub-basenji-adult-standing?w=320&fm=jpg&auto=compress",
+    "descripcion": "Los basenjis no ladran, sino que emiten un tipo muy particular de canto yodel. Los basenjis son perros de contextura delgada y huesos finos con patas largas, siempre bien equilibrados, animados e inteligentes.",
+    "acercade": "Aunque es relativamente independiente y distante con los extraños, el basenji es conocido por ser afectuoso con su familia. Estos perros notablemente limpios son activos y de movimiento grácil, ya que exhiben zancadas largas y suaves.",
+    "pais": "Francia",
+    "tamaño": "Mediano",
+    "vida": "12-16 años"
+},
+{
+    "id": 2,
+    "raza": "Ariegeois",
+    "thumbnail": "https://cdn.royalcanin-weshare-online.io/22k9HWsBG95Xk-RBlfiM/v1/bd26bl-hub-ariegeois-adult-standing?w=320&fm=jpg&auto=compress",
+    "descripcion": "Los Ariegeois son perros trabajadores, muy inteligentes y resistentes, lo que los convierte en excelentes perros de caza. Tienen una personalidad alegre y sociable; los caracteriza una lealtad inquebrantable.",
+    "acercade": "El Ariegeois tiene las características físicas de un sabueso. Es famoso por sus habilidades, su obediencia, ya sea solo o en manada, y su agilidad en terrenos difíciles.",
+    "pais": "Francia",
+    "tamaño": "Mediano",
+    "vida": "10-12 años"
+},
+{
+    "id": 3,
+    "raza": "American Stanffordshire Terrier",
+    "thumbnail": "https://cdn.royalcanin-weshare-online.io/OiLoHWsBaxEApS7LKR24/v1/bd126h-hub-american-staffordshire-terrie-adult-black-and-white?fm=jpg&auto=compress",
+    "descripcion": "El American Staffordshire Terrier, AST o Amstaff es una raza canina originaria de Estados Unidos. Esta raza es descendiente del American Pitbull Terrier.",
+    "acercade": "El American Staffordshire terrier, fuerte, atlético y valiente, es ideal para entrenamientos de obediencia y agilidad. El American Staffordshire terrier es una raza muy afectuosa y sociable con las personas. Ama pasar el tiempo con su compañero humano.",
+    "pais": "Estados Unidos",
+    "tamaño": "Mediano",
+    "vida": "12-14 años"
+},
+{
+    "id": 4,
+    "raza": "Bichón Maltés",
+    "thumbnail": "https://cdn.royalcanin-weshare-online.io/0mksHWsBG95Xk-RBvfih/v1/bd16bl-hub-maltese-adult-standing?w=320&fm=jpg&auto=compress",
+    "descripcion": "Animados, afectuosos y muy inteligentes, los bichones malteses son excelentes perros de compañía.",
+    "acercade": "Los bichones malteses son amistosos y dóciles, y saludarán a las personas con confianza. Durante siglos han sido criados como compañeros y nunca son más felices que cuando están sentados en el regazo de su dueño. A pesar del nombre, los perros de esta raza no son de Malta en absoluto. La palabra deriva de una palabra en uno de los idiomas mediterráneos para refugio o puerto, que simplemente también es la raíz de la palabra Malta.",
+    "pais": "Estados Unidos",
+    "tamaño": "Mediano",
+    "vida": "12–15 años"
+},
+{
+    "id": 5,
+    "raza": "Basset Leonado de Bretaña",
+    "thumbnail": "https://cdn.royalcanin-weshare-online.io/AiLQxWoBaxEApS7L7Bxa/v1/bd138bl-hub-basset-fauve-de-bretagne-adult-standing?w=640&fm=jpg&auto=compress",
+    "imagen": "./bd126h.jpg",
+    "descripcion": "El Basset Leonado de Bretaña es un perro de caza de tipo sabueso y tamaño medio originario de la Bretaña francesa y descendiente del Grifón leonado de Bretaña.",
+    "acercade": "Además de cazadores apasionados, los perros de esta raza son excelentes compañeros para las personas y son valorados por su sociabilidad, afecto y equilibrio. \n \n \n Les es fácil adaptarse a cualquier juego y cualquier terreno. Durante la caza, son valientes, astutos y obstinados, lo cual es una muy buena receta para la eficiencia.",
+    "pais": "Francia",
+    "tamaño": "Pequeño",
+    "vida": "11-14 años"
+},
+{
+    "id": 6,
+    "raza": "Bichón Boloñés",
+    "thumbnail": "https://cdn.royalcanin-weshare-online.io/u_pKHWsBIYfdNSoCOxqf/v1/bd33bl-hub-bolognese-adult-standing?w=320&auto=compress&fm=jpg",
+    "imagen": "https://cdn.royalcanin-weshare-online.io/vfpNHWsBIYfdNSoCqRqj/v1/bd33h-hub-bolognese-black-and-white?w=640&auto=compress&fm=jpg",
+    "descripcion": "La boloñesa es una raza de perro de tipo bichón originaria de la ciudad italiana de Bolonia. Una raza toy que se parece mucho al Maltés. ",
+    "acercade": "Estos perros tan entrañables, que tienen una expresión tan seria, son conocidos por su inteligencia, naturaleza observadora y disposición tranquila. La personalidad cómica y naturaleza curiosa los convierten en perros familiares muy queridos. Ya sea acurrucado en casa o paseando, el bichón boloñés es un perro devoto al que lo que más le gusta es estar cerca de la familia humana.",
+    "pais": "Italia",
+    "tamaño": "Muy Pequeño",
+    "vida": "12-14 años"
+}
+
+]
+
+export function getAll() {
+return perros;
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import Home from './pages/home/Home';
+import Perros from './pages/perros/Sperros';
+import Error404 from './pages/error404/Error404';
+
+import { BrowserRouter } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path='/perros' element={<Perros/>}></Route>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Route path='*' element={<Error404 />}></Route>
+    <Route path='/home' element={<Navigate replace to={"/"} />}></Route>
+  </Routes>
+  </BrowserRouter>
+);
